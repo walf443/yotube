fead69546b93f535a52c3ef9c10733f572108aa7
Keiji Yoshimi <walf443@gmail.com>
2018-11-03 22:35:31 +0900

    added 5A page

---
+++ app/pages/5A/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+      <Category :category="category"/>
+  </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import {findCategoryByName, utils} from "../../utils/trick"
+import Category from "../../components/Category"
+
+@Component({
+  components: {
+    Category
+  }
+})
+export default class extends Vue {
+  @State categories;
+  get category (): utils.Category | null {
+    return findCategoryByName(this.categories, '5A');
+  }
+}
+</script>
+<style scoped>
+
+</style>
