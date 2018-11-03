f40a30ca5135f35286dca1c8ba84fcc18afd3f2e
Keiji Yoshimi <walf443@gmail.com>
2018-11-03 21:35:06 +0900

    support all 1A pages.

---
+++ app/pages/1A/level5.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+      <Level :category="category" :level="level" :index="levelIndex" isExpanded="true" />
+  </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import {utils, findCategoryByName, findCategoryLevel} from "../../utils/trick"
+import Level from "../../components/Level"
+
+@Component({
+  components: {
+      Level
+  }
+})
+export default class extends Vue {
+  @State categories;
+  get category(): utils.Category | null {
+      return findCategoryByName(this.categories, '1A');
+  }
+
+  get level (): utils.Level | null {
+    return findCategoryLevel(this.categories, '1A', this.levelIndex);
+  }
+
+  get levelIndex(): number {
+      return 4;
+  }
+}
+</script>
