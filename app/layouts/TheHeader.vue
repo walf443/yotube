b4501510c1f606997ace922092a6124cbd8d0359
Keiji Yoshimi <walf443@gmail.com>
2018-11-17 15:54:08 +0900

    move category link to level1

--- app/layouts/TheHeader.vue
+++ app/layouts/TheHeader.vue
@@ -3,20 +3,20 @@
         <el-menu-item index="1" @click="$router.push('/')">
             <nuxt-link id="TheLogo" :to="{ path: '/' }">Yoyotube</nuxt-link>
         </el-menu-item>
-        <el-menu-item index="2" @click="$router.push('/1A')">
-            <nuxt-link :to="{ path: '/1A' }">1A</nuxt-link>
+        <el-menu-item index="2" @click="$router.push('/1A/level1')">
+            <nuxt-link :to="{ path: '/1A/level1' }">1A</nuxt-link>
         </el-menu-item>
-        <el-menu-item index="3" @click="$router.push('/2A')">
-            <nuxt-link :to="{ path: '/2A' }">2A</nuxt-link>
+        <el-menu-item index="3" @click="$router.push('/2A/level1')">
+            <nuxt-link :to="{ path: '/2A/level1' }">2A</nuxt-link>
         </el-menu-item>
-        <el-menu-item index="4" @click="$router.push('/3A')">
-            <nuxt-link :to="{ path: '/3A' }">3A</nuxt-link>
+        <el-menu-item index="4" @click="$router.push('/3A/level1')">
+            <nuxt-link :to="{ path: '/3A/level1' }">3A</nuxt-link>
         </el-menu-item>
-        <el-menu-item index="5" @click="$router.push('/4A')">
-            <nuxt-link :to="{ path: '/4A' }">4A</nuxt-link>
+        <el-menu-item index="5" @click="$router.push('/4A/level1')">
+            <nuxt-link :to="{ path: '/4A/level1' }">4A</nuxt-link>
         </el-menu-item>
-        <el-menu-item index="6" @click="$router.push('/5A')">
-            <nuxt-link :to="{ path: '/5A' }">5A</nuxt-link>
+        <el-menu-item index="6" @click="$router.push('/5A/level1')">
+            <nuxt-link :to="{ path: '/5A/level1' }">5A</nuxt-link>
         </el-menu-item>
     </el-menu>
 </template>
