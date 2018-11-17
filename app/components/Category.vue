ad4e3d5cbe98926d05900c04bef0aef681727c43
Keiji Yoshimi <walf443@gmail.com>
2018-11-17 16:39:32 +0900

    remove category top pages.

--- app/components/Category.vue
+++ app/components/Category.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h2><nuxt-link :to="{ 'path': `/${category.name}` }">{{ category.name }}</nuxt-link></h2>
+      <h2><nuxt-link :to="{ 'path': `/${category.name}/level1` }">{{ category.name }}</nuxt-link></h2>
     <div v-for="(level, index) in category.levels" :key=level.id>
         <Level :index="index" :level="level" :category="category" />
     </div>
