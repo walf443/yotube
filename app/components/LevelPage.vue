8cd856ec9480e2d3e4037f6c21c6ec44bf5f943a
Keiji Yoshimi <walf443@gmail.com>
2018-11-19 00:44:30 +0900

    added navigation to prev/next level

--- app/components/LevelPage.vue
+++ app/components/LevelPage.vue
@@ -1,6 +1,22 @@
 <template>
   <section>
+      <div class="page-nav">
+          <div class="page-nav-link page-nav-left" v-if="prevLevel" @click="handleClickPrev">
+              <i class="el-icon-arrow-left" /> {{prevLevelName}}
+          </div>
+          <div class="page-nav-link page-nav-right" v-if="nextLevel" @click="handleClickNext">
+              {{nextLevelName}} <i class="el-icon-arrow-right" />
+          </div>
+      </div>
       <Level :category="category" :level="level" :index="levelIndex" isExpanded="true" />
+      <div class="page-nav">
+          <div class="page-nav-link page-nav-left" v-if="prevLevel" @click="handleClickPrev">
+             <i class="el-icon-arrow-left" /> {{prevLevelName}}
+          </div>
+          <div class="page-nav-link page-nav-right" v-if="nextLevel" @click="handleClickNext">
+              {{nextLevelName}} <i class="el-icon-arrow-right" />
+          </div>
+      </div>
   </section>
 </template>
 
@@ -34,6 +50,22 @@ export default class LevelPage extends Vue {
     return findCategoryLevel(this.categories, this.categoryName, this.levelIndex);
   }
 
+  get prevLevelName(): string | null {
+      return this.getLevelName(this.levelIndex - 1);
+  }
+
+  get nextLevelName(): string | null {
+      return this.getLevelName(this.levelIndex + 1);
+  }
+
+  get prevLevel(): utils.Level | null {
+      return findCategoryLevel(this.categories, this.categoryName, this.levelIndex - 1);
+  }
+
+  get nextLevel(): utils.Level | null {
+      return findCategoryLevel(this.categories, this.categoryName, this.levelIndex + 1);
+  }
+
   get categoryName(): string {
       throw 'please override';
   }
@@ -41,5 +73,62 @@ export default class LevelPage extends Vue {
   get levelIndex(): number {
       throw 'please override';
   }
+
+  getLevelName(index: number): string {
+      return `レベル${index + 1}`;
+  }
+
+  getLevelLink(index: number): string | null {
+      const category = this.category;
+      if (category == null) {
+          return null;
+      }
+
+      return `/${category.name}/level${index + 1}`;
+  }
+
+  handleClickPrev() {
+      const link = this.getLevelLink(this.levelIndex - 1);
+      if (link == null) {
+          return;
+      }
+      this.$router.push(link);
+  }
+  handleClickNext() {
+      const link = this.getLevelLink(this.levelIndex + 1);
+      if (link == null) {
+          return;
+      }
+      this.$router.push(link);
+  }
 }
 </script>
+
+<style scoped>
+    .page-nav {
+        width: 100%;
+        height: 20px;
+    }
+
+    .page-nav::after {
+        content: ' ';
+        clear: both;
+    }
+
+    .page-nav-link {
+        color: #666;
+    }
+
+    .page-nav-link:hover {
+        cursor: pointer;
+        text-decoration: underline;
+    }
+
+    .page-nav-left {
+      float: left;
+    }
+
+    .page-nav-right {
+      float: right;
+    }
+</style>
