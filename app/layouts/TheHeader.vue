cfd2670bfe5edef4e733c0df49aeac31616861f7
Keiji Yoshimi <walf443@gmail.com>
2019-05-05 18:04:56 +0900

    do not use el-menu

--- app/layouts/TheHeader.vue
+++ app/layouts/TheHeader.vue
@@ -1,24 +1,24 @@
 <template>
-    <el-menu mode="horizontal">
-        <el-menu-item index="1" @click="$router.push('/')">
+    <div class="menu">
+        <div class="menu-item-logo menu-item" index="1" @click="$router.push('/')">
             <nuxt-link id="TheLogo" :to="{ path: '/' }">Yo tube</nuxt-link>
-        </el-menu-item>
-        <el-menu-item index="2" @click="$router.push('/1A/level1')">
+        </div>
+        <div class="menu-item" index="2" @click="$router.push('/1A/level1')">
             <nuxt-link :to="{ path: '/1A/level1' }">1A</nuxt-link>
-        </el-menu-item>
-        <el-menu-item index="3" @click="$router.push('/2A/level1')">
+        </div>
+        <div class="menu-item" index="3" @click="$router.push('/2A/level1')">
             <nuxt-link :to="{ path: '/2A/level1' }">2A</nuxt-link>
-        </el-menu-item>
-        <el-menu-item index="4" @click="$router.push('/3A/level1')">
+        </div>
+        <div class="menu-item" index="4" @click="$router.push('/3A/level1')">
             <nuxt-link :to="{ path: '/3A/level1' }">3A</nuxt-link>
-        </el-menu-item>
-        <el-menu-item index="5" @click="$router.push('/4A/level1')">
+        </div>
+        <div class="menu-item" index="5" @click="$router.push('/4A/level1')">
             <nuxt-link :to="{ path: '/4A/level1' }">4A</nuxt-link>
-        </el-menu-item>
-        <el-menu-item index="6" @click="$router.push('/5A/level1')">
+        </div>
+        <div class="menu-item" index="6" @click="$router.push('/5A/level1')">
             <nuxt-link :to="{ path: '/5A/level1' }">5A</nuxt-link>
-        </el-menu-item>
-    </el-menu>
+        </div>
+    </div>
 </template>
 <script>
     export default {
@@ -33,15 +33,26 @@
         text-decoration: none;
     }
 
-    .el-menu-item a {
+    .menu {
+        display: flex;
+        align-items: center;
+    }
+
+    .menu-item:hover {
+        cursor: pointer;
+    }
+
+    .menu-item-logo {
+        width: 120px;
+    }
+
+    .menu-item > a {
+        padding: 16px;
         text-decoration: none;
     }
 
     @media screen and (max-width: 450px) {
-        #TheLogo {
-            padding: 16px;
-        }
-        .el-menu-item {
+        .menu-item > a {
             padding: 12px;
         }
     }
